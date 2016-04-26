600f6ecb7df7f96fb1017ded311226221dc99935
agdavid <antonio.g.david@gmail.com>
2016-04-26 08:58:42 -0400

    add lists#index and #create action, views/index with new list form

--- NOTES.md
+++ NOTES.md
@@ -1,7 +1,7 @@
 Step 1: CRUD Lists
-- 1(a) allow user to see all their lists
+[x] 1(a) allow user to see all their lists
   - model: list
-  - controller: lists#index
+  - controller: lists#index, lists#create
   - views: lists/index.html.erb
 
 - 1(b) allow user to see a specific list
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,21 @@
 class ListsController < ApplicationController
+
+  def index #doubles as #new action
+    @list = List.new #stub out new @list for form_for FormHelper
+    @lists = List.all #query db for all @lists
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      redirect_to lists_path
+    end
+  end
+
+  private
+    def list_params
+      params.require(:list).permit(:name)
+    end
 end
